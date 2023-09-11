a490dbfcb2abdaba61fd19df6aa0bece7b04a0c3
Aloisio Magalhaes <educacao1ouro@gmail.com>
2023-09-11 14:48:03 -0300

    header php adicionados

--- src/index.php
+++ src/index.php
@@ -3,7 +3,17 @@ require('../vendor/autoload.php');
 require_once('../index.html');
 header('Content-Language: pt-BR');
 header('Content-Type: text/html; charset=UTF-8');
+header('X-XSS-Protection: 1; mode=block');
+header('Cache-Control: stale-while-revalidate=259200');
+header('X-Frame-Options: DENY');
+header('Strict-Transport-Security: max-age=31536000');
+header('X-UA-Compatible: IE=edge,chrome=1');
+header('X-Robots-Tag: noindex');
+header('X-Download-Options: noopen');
+header('X-Permitted-Cross-Domain-Policies: none');
 header('X-Content-Type-Options: nosniff');
+header('X-DNS-Prefetch-Control: off');
+header('viewport: maximum-scale=1, shrink-to-fit=no');
 
 $icon_menu = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>';
 $icon_calendario = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
