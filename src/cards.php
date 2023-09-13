837fdab7d8c884931beedb0483e026c06fc018a1
Aloisio Magalhaes <educacao1ouro@gmail.com>
2023-09-13 16:19:26 -0300

    svg icons otimizado

---
+++ src/cards.php
@@ -0,0 +1,17 @@
+<!-- CARD-IMG-TASK -->
+<div data-draggable="true" class="card img-task">
+    <div class="card-body">
+        <div class="task-content"><img src="assets/img/400x168.jpg" class="img-fluid" alt="scrumboard"></div>
+        <div class="task-header">
+            <div>
+                <h4 data-tasktitle="Creating a new Portfolio on Dribble">Creating a new Portfolio on Dribble</h4>
+            </div>
+        </div>
+        <div class="task-body">
+            <div class="task-bottom">
+                <div class="tb-section-1"><span data-taskdate="08 Aug 2020"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"/><path d="M16 2v4M8 2v4m-5 4h18"/></svg>08 Aug, 2020</span></div>
+                <div class="tb-section-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 s-task-edit"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 s-task-delete"><path d="M3 6h18m-2 0v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2m-6 5v6m4-6v6"/></svg></div>
+            </div>
+        </div>
+    </div>
+</div>
